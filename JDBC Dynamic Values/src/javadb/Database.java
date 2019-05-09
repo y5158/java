2868831f1b0fc81ba34c309b9e4151ae97adea9c
ditiss <ditiss@DITISS-PC29>
2019-05-09 15:56:26 +0530

    hi

---
+++ JDBC Dynamic Values/src/javadb/Database.java
@@ -0,0 +1,31 @@
+package javadb;
+import java.sql.*;
+import java.util.Scanner;
+
+public class Database {
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		System.out.println("\nDrivers loaded");
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost/test" , "root", "");
+		System.out.println("\nConnection Established ");
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter id:");
+		int id=scan.nextInt();
+		scan.nextLine();
+		System.out.println("Enter Name:");
+		String name=scan.nextLine();
+		
+		System.out.println("Enter Address:");
+		String address=scan.nextLine();
+		
+		String sql="insert into stud values(?,?,?)";
+		PreparedStatement pst=con.prepareStatement(sql);
+		pst.setInt(1,id);
+		pst.setString(2,name);
+		pst.setString(3, address);
+		
+		int i=pst.executeUpdate();
+		System.out.println("Rows affected:" +i);
+	}
+
+}
