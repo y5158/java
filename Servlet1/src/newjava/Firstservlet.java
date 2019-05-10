11f66d48b69d3fad2da32364228f8c1295212b4b
ditiss <ditiss@DITISS-PC29>
2019-05-10 12:18:34 +0530

    hi

---
+++ Servlet1/src/newjava/Firstservlet.java
@@ -0,0 +1,28 @@
+package newjava;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/Firstservlet")
+public class Firstservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		out.println("<h1>Welcoem to servlet</h1>");
+	}
+
+}
+
+
+
